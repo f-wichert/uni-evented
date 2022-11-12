7b82da03c63797329d61928920e5723d56ed1e68
Fred <f.wichert.d@posteo.net>
2022-11-12 17:17:37 +0100

    added basic navigation and folder structure. all screens go into screens folder and all reusable components in the components folder

--- Evented/App.tsx
+++ Evented/App.tsx
@@ -1,15 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { NavigationContainer, TypedNavigator } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import LoginScreen from './screens/LoginScreen';
+import ViewEventScreen from './screens/ViewEventScreen';
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function TabScreen(params:type) {
+  
 }
 
+export default function App() {
+    return (
+        // <Provider store={store}>
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen
+                    name="LoginScreen"
+                    component={LoginScreen}
+                    // options={{ headerShown: false }}
+                />
+                <Stack.Screen 
+                    name="EventScreen"
+                    component={ViewEventScreen}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+        {/* </Provider> */}
+    );
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
---
+++ Evented/screens/ViewEventScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function ViewEventScreen(props) {
+    return (
+        <View>
+            <Text>
+                View Event Screen!
+            </Text>
+        </View>
+    );
+}
+
+export default ViewEventScreen;
