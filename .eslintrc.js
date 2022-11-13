e4693fe59e0b0cbe87bf5c27ca155a212f51cee1
Simon J <simon.jungherz@stud.tu-darmstadt.de>
2022-11-13 20:18:32 +0000

    Add lint/format deps and configs

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    env: {
+        node: true,
+        es2020: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:react/recommended',
+        'prettier',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ['./tsconfig.json'],
+    },
+    plugins: ['@typescript-eslint'],
+    root: true
+};
