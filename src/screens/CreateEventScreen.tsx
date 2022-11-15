592604de024a678925ef55c10c026f352c629977
jonas.milkovits <jonas.milkovits@stud.tu-darmstadt.de>
2022-11-15 09:28:47 +0100

    create create-event-screen

---
+++ src/screens/CreateEventScreen.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {StyleSheet, View, Text } from 'react-native';
+
+function CreateEventScreen() {
+    return (
+        <View>
+            <Text>Login Screen!</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    
+});
+
+export default CreateEventScreen;
