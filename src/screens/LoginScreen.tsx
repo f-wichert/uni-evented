48eda48a497e97c4512a5fdbcc0d75dde9860626
Fred <f.wichert.d@posteo.net>
2022-11-20 12:19:07 +0100

    Changed styling on login screen. This could be done way better and split into more components, but the components would make it more complex. I will leave it as is right now and maybe rework it on the signup screen

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -2,6 +2,8 @@ import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { useContext, useState } from 'react';
 import { Image, StyleSheet, Text, TextInput, TouchableHighlight, View } from 'react-native';
 
+import Ionicons from '@expo/vector-icons/Ionicons';
+
 import yellowSplash from '../../assets/yellow_splash.png';
 import { RootNavigatorParams } from '../App';
 import { Context as AuthContext } from '../contexts/authContext';
@@ -14,7 +16,7 @@ function LoginScreen({ navigation }: ComponentProps) {
     console.log(`log: ${user}`);
     // TODO: require these to be non-empty in the UI
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    await signin({ email: user!, password: password! });
+    // await signin({ email: user!, password: password! });
     navigation.navigate('TabScreen');
   }
 
@@ -25,6 +27,7 @@ function LoginScreen({ navigation }: ComponentProps) {
   return (
     <View style={styles.container}>
       <Image style={styles.art} source={yellowSplash} />
+      
       <View style={styles.headerText}>
         <Text
           style={{
@@ -34,9 +37,26 @@ function LoginScreen({ navigation }: ComponentProps) {
         >
           Login
         </Text>
+        <Text style={{
+          fontSize: 15,
+          fontWeight: 'bold',
+          color: '#bdbdbd'
+        }}>
+          Please sign in to continue.
+        </Text>
       </View>
-      <TextInput style={styles.userInput} onChangeText={setUser} />
-      <TextInput style={styles.passwordInput} secureTextEntry={true} onChangeText={setPassword} />
+      
+      <View style={{...styles.userInputBox, ...styles.elevation}}>
+        <Ionicons name={'person-outline'} size={20} color={'black'} />
+        <TextInput 
+          style={styles.userInput} 
+          onChangeText={setUser} 
+          onFocus={() => console.log('focused')}/>
+      </View>
+      <View style={{...styles.passwInputBox, ...styles.elevation}}>
+        <Ionicons name={'lock-closed-outline'} size={20} color={'black'} />
+        <TextInput style={styles.passwordInput} secureTextEntry={true} onChangeText={setPassword} />
+      </View>
 
       <TouchableHighlight
         style={styles.loginButton}
@@ -52,6 +72,27 @@ function LoginScreen({ navigation }: ComponentProps) {
           Join!
         </Text>
       </TouchableHighlight>
+
+      <View style={styles.footerText}>
+        <Text style={{
+          fontSize: 15,
+          fontWeight: 'bold',
+          color: '#bdbdbd'
+          }}
+        >
+          Don't have an account?
+        </Text>
+        <Text style={{
+          fontSize: 15,
+          fontWeight: 'bold',
+          color: '#D9B611'
+          }}
+        >
+          Sign up
+        </Text>
+      </View>
+
+
     </View>
   );
 }
@@ -71,23 +112,48 @@ const styles = StyleSheet.create({
     // backgroundColor: 'red'
     // alignItems: 'center'
   },
+  userInputBox: {
+    height: 40,
+    width: 300,
+    flexDirection: 'row',
+    // justifyContent: 'center'
+    paddingLeft: 5,
+    alignItems: 'center',
+    marginBottom: 20,
+    // borderWidth: 1,
+    borderRadius: 8,
+    backgroundColor: '#efefef'
+  },
+  passwInputBox: {
+    height: 40,
+    width: 300,
+    flexDirection: 'row',
+    // justifyContent: 'center'
+    paddingLeft: 5,
+    alignItems: 'center',
+    marginBottom: 20,
+    // borderWidth: 1,
+    borderRadius: 8,
+    backgroundColor: '#efefef'
+  },
   userInput: {
     height: 40,
     width: 200,
-    marginBottom: 10,
-    borderWidth: 1,
-    borderRadius: 5,
+    // marginBottom: 10,
+    // borderWidth: 1,
+    // borderRadius: 5,
   },
   passwordInput: {
     height: 40,
     width: 200,
-    marginBottom: 10,
-    borderWidth: 1,
-    borderRadius: 5,
+    // marginBottom: 10,
+    // borderWidth: 1,
+    // borderRadius: 5,
   },
   headerText: {
     height: 80,
     marginTop: 200,
+    marginBottom: 70,
     justifyContent: 'center',
     // backgroundColor: 'red'
   },
@@ -96,8 +162,9 @@ const styles = StyleSheet.create({
     height: 40,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#fcba03',
+    backgroundColor: '#D9B611',
     borderRadius: 20,
+    marginBottom: 270
   },
   art: {
     height: 260,
@@ -105,6 +172,21 @@ const styles = StyleSheet.create({
     position: 'absolute',
     transform: [{ translateX: 80 }, { rotate: '-120deg' }],
   },
+  shadowProp: {
+    shadowColor: '#52006A',
+    shadowOffset: {width: -2, height: 4},
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+  },
+  elevation: {
+    elevation: 15,
+    shadowColor: '#71717',
+  },
+  footerText: {
+    justifyContent: 'center',
+    flexDirection: 'row',
+    // bottom: 
+  }
 });
 
 export default LoginScreen;
