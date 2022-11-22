f6dd945f2a91a153904f940789f8599e2a720def
jonas.milkovits <jonas.milkovits@stud.tu-darmstadt.de>
2022-11-22 09:07:12 +0100

    add map screen and render marker at home location

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -1,21 +1,66 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useContext, useEffect, useRef } from 'react';
+import { Alert, Button, Dimensions, StyleSheet, TextInput, View, Text, Platform, TouchableOpacity } from 'react-native';
+import { Context as AuthContext } from '../contexts/authContext';
+import MapView, { Marker } from 'react-native-maps';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import * as Location from 'expo-location';
 
 function MapScreen() {
-  return (
-    <View style={styles.container}>
-      <Text>Map Screen</Text>
-    </View>
-  );
+
+    // Location State
+    const [location, setLocation] = useState(null);
+    const [locationReady, setLocationReady] = useState(false);
+
+    useEffect(() => {
+        (async () => {
+
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                setErrorMsg('Permission to access location was denied');
+                return;
+            }
+
+            let location = await Location.getCurrentPositionAsync({});
+            console.log(location);
+            setLocation(location);
+            setLocationReady(true);
+        })();
+    }, []);
+
+    return (
+        <View style={styles.container}>
+            {locationReady ?
+                <MapView
+                    initialRegion={{ latitude: location.coords.latitude, longitude: location.coords.longitude, latitudeDelta: 0.01, longitudeDelta: 0.01 }}
+                    style={styles.map}
+                >
+                    <Marker
+                        key={1}
+                        coordinate={{
+                            latitude: location.coords.latitude,
+                            longitude: location.coords.longitude,
+                        }}
+                        title='You'
+                    />
+                </MapView >
+                : null
+            }
+
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    map: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
+    },
 });
 
 export default MapScreen;
