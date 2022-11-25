bf74a6eb96be8e38c214604925a35ab85ff6e8d2
Simon J <simon.jungherz@stud.tu-darmstadt.de>
2022-11-25 16:55:10 +0000

    Extract tab navigator to separate file

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
-import Ionicons from '@expo/vector-icons/Ionicons';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import React, { ReactNode, useContext } from 'react';
+import React, { ReactNode } from 'react';
 import { ToastProvider } from 'react-native-toast-notifications';
 
 import ToastRoot from './components/ToastRoot';
-import { Context as AuthContext, Provider as AuthProvider } from './contexts/authContext';
-import CreateEventScreen from './screens/CreateEventScreen';
-import DiscoverScreen from './screens/DiscoverScreen';
+import { Provider as AuthProvider } from './contexts/authContext';
+import TabNavigator from './nav/TabNavigator';
 import LoginScreen from './screens/LoginScreen';
-import MapScreen from './screens/MapScreen';
 import RegisterScreen from './screens/RegisterScreen';
-import ViewEventScreen from './screens/ViewEventScreen';
-import { IoniconsName } from './types';
 
 // https://reactnavigation.org/docs/typescript/
 // instead of `undefined`, props passed to these screens would be defined here if applicable
@@ -25,59 +19,6 @@ export type RootNavigatorParams = {
 };
 
 const Stack = createNativeStackNavigator<RootNavigatorParams>();
-
-export type TabNavigatorParams = {
-    Discover: undefined;
-    Create: undefined;
-    Map: undefined;
-    Event: undefined;
-};
-
-const Tab = createBottomTabNavigator<TabNavigatorParams>();
-
-function TabScreen() {
-    const { state } = useContext(AuthContext);
-
-    return (
-        <Tab.Navigator
-            screenOptions={({ route }) => ({
-                tabBarIcon: ({ focused, color, size }) => {
-                    let iconName: IoniconsName;
-
-                    if (route.name === 'Discover') {
-                        iconName = focused ? 'play' : 'play-outline';
-                        // } else if (route.name === 'Settings') {
-                        //   iconName = focused ? 'settings' : 'settings-outline';
-                    } else if (route.name === 'Map') {
-                        iconName = focused ? 'map' : 'map-outline';
-                        // } else if (route.name === 'Profile') {
-                        //   iconName = focused ? 'person' : 'person-outline';
-                    } else if (route.name === 'Create') {
-                        iconName = focused ? 'add' : 'add-outline';
-                    } else if (route.name === 'Event') {
-                        iconName = focused ? 'rocket' : 'rocket-outline';
-                    } else {
-                        throw new Error(`Unknown route '${route.name}'`);
-                    }
-
-                    // You can return any component that you like here!
-                    return <Ionicons name={iconName} size={size} color={color} />;
-                },
-                tabBarActiveTintColor: 'tomato',
-                tabBarInactiveTintColor: 'gray',
-            })}
-            initialRouteName={state.eventActive ? 'Event' : 'Discover'}
-        >
-            <Tab.Screen name="Discover" component={DiscoverScreen} />
-            {state.eventActive ? (
-                <Tab.Screen name="Event" component={ViewEventScreen} />
-            ) : (
-                <Tab.Screen name="Create" component={CreateEventScreen} />
-            )}
-            <Tab.Screen name="Map" component={MapScreen} />
-        </Tab.Navigator>
-    );
-}
 
 // wrapper for providers and similar root components,
 // to avoid constantly having to indent the app hierarchy below further
@@ -106,12 +47,12 @@ export default function App() {
                     />
                     <Stack.Screen
                         name="TabScreen"
-                        component={TabScreen}
+                        component={TabNavigator}
                         options={{ headerShown: false }}
                     />
-                    {/* <Stack.Screen name="EventScreen" component={ViewEventScreen} /> */}
                 </Stack.Navigator>
             </NavigationContainer>
+
             <ToastRoot />
         </RootWrapper>
     );
---
+++ src/nav/TabNavigator.tsx
@@ -0,0 +1,65 @@
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import React, { useContext } from 'react';
+
+import { Context as AuthContext } from '../contexts/authContext';
+import CreateEventScreen from '../screens/CreateEventScreen';
+import DiscoverScreen from '../screens/DiscoverScreen';
+import MapScreen from '../screens/MapScreen';
+import ViewEventScreen from '../screens/ViewEventScreen';
+import { IoniconsName } from '../types';
+
+// https://reactnavigation.org/docs/typescript/
+
+export type TabNavigatorParams = {
+    Discover: undefined;
+    Create: undefined;
+    Map: undefined;
+    Event: undefined;
+};
+
+export const Tab = createBottomTabNavigator<TabNavigatorParams>();
+
+export default function TabNavigator() {
+    const { state } = useContext(AuthContext);
+
+    return (
+        <Tab.Navigator
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color, size }) => {
+                    let iconName: IoniconsName;
+
+                    if (route.name === 'Discover') {
+                        iconName = focused ? 'play' : 'play-outline';
+                        // } else if (route.name === 'Settings') {
+                        //   iconName = focused ? 'settings' : 'settings-outline';
+                    } else if (route.name === 'Map') {
+                        iconName = focused ? 'map' : 'map-outline';
+                        // } else if (route.name === 'Profile') {
+                        //   iconName = focused ? 'person' : 'person-outline';
+                    } else if (route.name === 'Create') {
+                        iconName = focused ? 'add' : 'add-outline';
+                    } else if (route.name === 'Event') {
+                        iconName = focused ? 'rocket' : 'rocket-outline';
+                    } else {
+                        throw new Error(`Unknown route '${route.name}'`);
+                    }
+
+                    // You can return any component that you like here!
+                    return <Ionicons name={iconName} size={size} color={color} />;
+                },
+                tabBarActiveTintColor: 'tomato',
+                tabBarInactiveTintColor: 'gray',
+            })}
+            initialRouteName={state.eventActive ? 'Event' : 'Discover'}
+        >
+            <Tab.Screen name="Discover" component={DiscoverScreen} />
+            {state.eventActive ? (
+                <Tab.Screen name="Event" component={ViewEventScreen} />
+            ) : (
+                <Tab.Screen name="Create" component={CreateEventScreen} />
+            )}
+            <Tab.Screen name="Map" component={MapScreen} />
+        </Tab.Navigator>
+    );
+}
