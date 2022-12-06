12f07b06362daa1c198109703805bf7c77fb6150
Dead_Cowboy <laurenz.kammeyer@gmx.de>
2022-12-06 19:58:01 +0100

    Resolve merge conflicts, again

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -20,101 +20,6 @@ export default function LoginScreen({ navigation }: ComponentProps) {
     const [password, setPassword] = useState<string | undefined>();
 
     return (
-<<<<<<< HEAD
-        <View style={styles.container}>
-            <Image style={styles.art} source={yellowSplash} />
-
-            <View style={styles.textBlock}>
-                <View style={styles.headerText}>
-                    <Text
-                        style={{
-                            fontSize: 44,
-                            fontWeight: 'bold',
-                        }}
-                    >
-                        Login
-                    </Text>
-                    <Text
-                        style={{
-                            fontSize: 15,
-                            fontWeight: 'bold',
-                            color: '#bdbdbd',
-                        }}
-                    >
-                        Please sign in to continue.
-                    </Text>
-                </View>
-            </View>
-
-            <View style={styles.dataBlock}>
-                <View style={{ ...styles.userInputBox, ...styles.elevation }}>
-                    <Ionicons name={'person-outline'} size={20} color={'black'} />
-                    <TextInput
-                        style={styles.userInput}
-                        onChangeText={setUser}
-                        onFocus={() => console.log('focused')}
-                        // TODO! Remove bevore deployment! Only for ease of Login
-                        defaultValue = 'Bob' 
-                        />
-                </View>
-                <View style={{ ...styles.passwInputBox, ...styles.elevation }}>
-                    <Ionicons name={'lock-closed-outline'} size={20} color={'black'} />
-                    <TextInput 
-                        style={styles.passwordInput}
-                        secureTextEntry={true}
-                        onChangeText={setPassword}
-                        // TODO! Remove bevore deployment! Only for ease of Login
-                        defaultValue='Verysecure' 
-                        />
-                </View>
-
-                <TouchableHighlight
-                    style={styles.loginButton}
-                    // activeOpacity={0.6}
-                    onPress={asyncHandler(login)}
-                >
-                    <ImageBackground
-                        source={yellowSplash}
-                        resizeMode="contain"
-                        style={styles.image}
-                    >
-                        <Text
-                            style={{
-                                fontSize: 18,
-                                fontWeight: 'bold',
-                            }}
-                        >
-                            Join!
-                        </Text>
-                    </ImageBackground>
-                </TouchableHighlight>
-            </View>
-
-            <View style={styles.infoBlock}>
-                <View style={styles.footerText}>
-                    <Text
-                        style={{
-                            fontSize: 15,
-                            fontWeight: 'bold',
-                            color: '#bdbdbd',
-                        }}
-                    >
-                        Don't have an account?
-                    </Text>
-                    <Text
-                        style={{
-                            fontSize: 15,
-                            fontWeight: 'bold',
-                            color: '#D9B611',
-                        }}
-                        onPress={() => navigation.navigate('RegisterScreen')}
-                    >
-                        Sign up
-                    </Text>
-                </View>
-            </View>
-        </View>
-=======
         <BaseLoginScreen
             fields={[
                 { icon: 'person-outline', onChange: setUser },
@@ -135,6 +40,5 @@ export default function LoginScreen({ navigation }: ComponentProps) {
                 callback: () => navigation.navigate('RegisterScreen'),
             }}
         />
->>>>>>> main
     );
 }
