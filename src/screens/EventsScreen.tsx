496e5a8db64dbc0fb2b799f036260b2e8a42a46c
jonas.milkovits <jonas.milkovits@stud.tu-darmstadt.de>
2022-12-06 22:59:54 +0100

    add new screens for navigation

---
+++ src/screens/EventsScreen.tsx
@@ -0,0 +1,17 @@
+import Ionicons from '@expo/vector-icons/Ionicons';
+import React, { useContext, useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+function EventsScreen() {
+
+    return (
+        <>
+
+        </>
+    );
+}
+
+const styles = StyleSheet.create({
+});
+
+export default EventsScreen;
---
+++ src/screens/ProfileScreen.tsx
@@ -0,0 +1,17 @@
+import Ionicons from '@expo/vector-icons/Ionicons';
+import React, { useContext, useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+function ProfileScreen() {
+
+    return (
+        <>
+
+        </>
+    );
+}
+
+const styles = StyleSheet.create({
+});
+
+export default ProfileScreen;
