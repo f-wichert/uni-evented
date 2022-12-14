ea4d46f6e802e0d63b57418828e6ff0fca99cfcf
Fred <f.wichert.d@posteo.net>
2022-12-14 10:31:49 +0100

    This seems harder than expected. I think this will eventually work, but it will still take a while

--- src/components/EventMarker.tsx
+++ src/components/EventMarker.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Image } from 'react-native';
 import MapView, { Marker, Callout } from 'react-native-maps';
+import WebView from 'react-native-webview';
 
 // TODO: kann mir jemand zeigen wie mann hier ein interface oder so für die props definiert? habe da schon unterschiedliche sachen gesehen - Fred
 
-function EventMarker(props) {
+function EventMarker({ coordinate, title, pinColor, onCalloutPress }) {
     return (
-        <Marker>
-            <Callout>
-                <View>
-
+        <Marker
+            coordinate={coordinate}
+            title={title}
+            pinColor={pinColor}
+            onCalloutPress={onCalloutPress}
+        >
+            <Callout 
+                style={{ height: 200 }}>
+                <View> 
+                    <WebView 
+                        style={{ height: 200, width: 200, }} 
+                        source={{ uri: 'https://reactnative.dev/' }} 
+                        /> 
+                    {/* <View style={styles.viewOrEnterDispensaryButtonContainer}> 
+                        <Text style={styles.viewOrEnterDispensaryButtonText}> {strings.dispensariesList.enterDispensary}</Text> 
+                    </View> */}
                 </View>
             </Callout>
         </Marker>
@@ -17,8 +30,14 @@ function EventMarker(props) {
 };
 
 const styles = StyleSheet.create({
-    container: {
+    calloutContainer: {
+        // width: 200,
+        // height: 200,
+        backgroundColor: 'red',
 
+        flexDirection: 'column',
+        // alignSelf: 'flex-start',
+        borderRadius: 6,
     },
     calloutTitle: {
 
@@ -31,6 +50,22 @@ const styles = StyleSheet.create({
     },
     calloutTag: {
         
+    },
+    arrow: {
+        backgroundColor: 'transparent',
+        borderColor: 'transparent',
+        borderTopColor: '#fff',
+        borderWidth: 16,
+        alignSelf: 'center',
+        marginTop: -32,
+    },
+    arrowBorder: {
+        backgroundColor: 'transparent',
+        borderColor: 'transparent',
+        borderTopColor: '#007a87',
+        borderWidth: 16,
+        alignSelf: 'center',
+        marginTop: -0.5
     }
 });
 
