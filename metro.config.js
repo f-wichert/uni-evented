dff48fa465c6c4fa64d0e2588df0cae3e6521397
sj <simon.jungherz@stud.tu-darmstadt.de>
2023-02-04 13:31:17 +0000

    fix: ignore all require cycle warnings

--- metro.config.js
+++ metro.config.js
@@ -3,8 +3,8 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// ignore import cycles for `src/state/*.ts` files, since those are generally fine
-config.resolver.requireCycleIgnorePatterns.push(/(^|\/)src\/state\/[^\/]+\.ts($|\/)/);
+// ignore import cycles
+config.resolver.requireCycleIgnorePatterns.push(/.*/);
 
 // exclude `node_modules/*/android` modules from fs watcher
 config.resolver.blockList = [/(^|\/)node_modules\/[^\/]+\/android\//];
